671b814b7ec9641b160cd168683be1e344310e73
bryanbriney <bryanbriney@Bryans-MacBook-Pro.local>
2016-01-01 19:26:52 -0800

    import fix

--- vaxtools/scPCR/__init__.py
+++ vaxtools/scPCR/__init__.py
@@ -1,2 +1,2 @@
 from vaxtools.scPCR.demultiplex import run as demultiplex
-from vaxtools.scPCR.excel_platemap_parser import run as parse_sample_info
+from vaxtools.scPCR.platemap import run as parse_sample_info
