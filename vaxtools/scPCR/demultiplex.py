03aeb22c5790d0f87becdda68af83b71ef62eee5
Sergey Menis <sergey.menis@gmail.com>
2018-10-11 15:38:12 -0700

    Added more detail to the error messages.

--- vaxtools/scPCR/demultiplex.py
+++ vaxtools/scPCR/demultiplex.py
@@ -357,13 +357,13 @@ def cdhit_clustering(seqs, bin_id, plate_name, num_plate_seqs, chain, args):
             cluster_seq_ids = [s.id for s in cdhit_result.largest_cluster.sequences]
             pixel_file = os.path.join(args.alignment_pixel_dir, '{}-{}_{}.png'.format(plate_name, bin_id, chain))
             pixel.make_pixel(seqs, pixel_file, consentroid=consentroid, cluster_seq_ids=cluster_seq_ids)
-        logger.info('PASSED')
+        logger.info('CLUSTER VALIDATION PASSED')
     else:
         consentroid = None
         if args.raw_sequence_dir is not None:
             logger.info('Writing raw sequences to file...')
             write_raw_sequences(seqs, plate_name, bin_id, chain, args)
-        logger.info('FALILED')
+        logger.info('CLUSTER VALIDATION FAILED')
     log_cluster_info(cdhit_result.clusters, passed, plate_name, bin_id, chain, len(seqs), consentroid, args)
     cdhit_result.delete()
     return consentroid
@@ -967,7 +967,7 @@ def main(args, logfile=None):
                 logger.info('Minimum well size: {}'.format(min_well_size))
                 logger.info('Total number of sequences: {}'.format(len(seqs)))
                 if len(seqs) < min_well_size:
-                    logger.info('FAILED')
+                    logger.info('FAILED: THE NUMBER OF SEQUENCES IS THE BELOW {}'.format(min_well_size))
                     continue
                 consentroid = cdhit_clustering(seqs, b, input_data.name, len(sequences), chain, args)
                 if consentroid is not None:
